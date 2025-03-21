b826ea84be39e3bccfe841fb732273e5615c6360
Rémi <remi.asselin42@gmail.com>
2025-03-21 16:03:08 +0100

    Ajout des variables d'environnement pour EmailJS dans la configuration de Vite et le workflow GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  define: {
+    "import.meta.env.VITE_EMAILJS_SERVICE_ID": JSON.stringify(
+      process.env.VITE_EMAILJS_SERVICE_ID
+    ),
+    "import.meta.env.VITE_EMAILJS_TEMPLATE_ID": JSON.stringify(
+      process.env.VITE_EMAILJS_TEMPLATE_ID
+    ),
+    "import.meta.env.VITE_EMAILJS_PUBLIC_KEY": JSON.stringify(
+      process.env.VITE_EMAILJS_PUBLIC_KEY
+    ),
+  },
 });
